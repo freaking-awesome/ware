7b383c20b44a63baeab7daae59fe4d3a54a73b03
Michael F <tehfreak@tehfreak.com>
2014-11-25 02:27:00 +0200

    v0.0.1

---
+++ test.js
@@ -0,0 +1,161 @@
+
+//
+// Подключение...
+//
+
+var Ware= require('./index') // require('ware')
+
+// приготовления
+
+function WTF(name) {
+    return new Error(name)
+}
+
+
+
+//
+// Использование...
+//
+
+var ware= new Ware // инстанцирование экземпляра системы
+
+
+
+//
+// Регистрация подпрограмм.
+//
+
+ware.use(function (a,b,c, next) { // регистрация подпрограммы
+    console.log('Выполнение подпрограммы №1...', this.arguments)
+    setTimeout(function () { // некая асинхронная операция:
+
+        if (Math.random() < .73) { // если повезет
+            next() // переход к следующей функции
+        } else { // иначе
+            var err= WTF('Поделил на ноль?')
+            next(err) // прерывание выполнения с ошибкой
+        }
+        console.log('Выполнение подпрограммы №1 завершено.\n')
+
+    }, 131) // 0.131 сек
+})
+
+ware.use(function (a,b,c, next) {
+    console.assert(this && this.arguments && this.arguments[0] === a && this.arguments[1] === b && this.arguments[2] === c)
+    this.next()
+})
+
+ware.use(function (a,b,c, next) {
+    console.log('Выполнение подпрограммы №3...')
+    setTimeout(function () {
+
+        if (Math.random() < .73) {
+            next()
+        } else {
+            var err= WTF('Поделил на ноль?')
+            next(err)
+        }
+
+        console.log('Выполнение подпрограммы №3 завершено.\n')
+    }, 131)
+})
+
+ware.use(function (a,b,c, next) {
+    next()
+})
+
+ware.use(function (a,b,c, next) {
+    console.log('Выполнение подпрограммы №5...')
+    setTimeout(function () {
+
+        if (Math.random() < .73) {
+            next()
+        } else {
+            var err= WTF('Поделил на ноль?')
+            next(err)
+        }
+
+        console.log('Выполнение подпрограммы №5 завершено.\n')
+    }, 131)
+})
+
+
+
+//
+// Композиция подпрограмм.
+//
+
+var ware1= new Ware // инстанцирование еще одного экземпляра системы
+
+ware.use(ware1) // композиция систем
+
+ware1.use(function (a,b,c, next) {
+    console.log('Выполнение подпрограммы №7...', this.arguments)
+    setTimeout(function () {
+
+        if (Math.random() < .73) {
+            next()
+        } else {
+            var err= WTF('Поделил на ноль?')
+            next(err)
+        }
+
+        console.log('Выполнение подпрограммы №7 завершено.\n')
+    }, 131)
+})
+
+ware1.use(function (a,b,c, next) {
+    next()
+})
+
+ware1.use(function (a,b,c, next) {
+    next()
+})
+
+ware1.use(function (a,b,c, next) {
+    next()
+})
+
+ware1.use(function (a,b,c, next) {
+    console.log('Выполнение подпрограммы №11...', this.arguments)
+    setTimeout(function () {
+
+        if (Math.random() < .73) {
+            next()
+        } else {
+            var err= WTF('Поделил на ноль?')
+            next(err)
+        }
+
+        console.log('Выполнение подпрограммы №11 завершено.\n')
+    }, 131)
+})
+
+var ware2= new Ware // инстанцирование еще одного экземпляра системы
+
+ware.use(ware2) // композиция систем
+
+ware2.use(function (a,b,c, next) {
+    console.log('Выполнение подпрограммы №13...', this.arguments)
+    setTimeout(function () {
+
+        if (Math.random() < .73) {
+            next()
+        } else {
+            var err= WTF('Поделил на ноль?')
+            next(err)
+        }
+
+        console.log('Выполнение подпрограммы №13 завершено.\n')
+    }, 131)
+})
+
+
+
+//
+// Выполнение подпрограмм.
+//
+
+ware.run(1, 2, 3, function (err) { // выполнение подпрограмм с указанными аргументами и последующий вызов указанной функции с ошибкой или без
+    console.log('Выполнение подпрограмм завершено.', err)
+})
